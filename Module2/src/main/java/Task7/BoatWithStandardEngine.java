aa038012f9373263e3ea25713ad6c2b3b60f2e6e
LizaSav <lisalizalisa@mail.ru>
2016-03-22 21:05:56 +0300

    add Task7

---
+++ Module2/src/main/java/Task7/BoatWithStandardEngine.java
@@ -0,0 +1,13 @@
+package Task7;
+
+import java.lang.annotation.Documented;
+
+/**
+ * Created by Elizaveta on 22.03.2016.
+ */
+@Documented
+public @interface BoatWithStandardEngine {
+    double horsepower = 10000;
+    String name ="sx200";
+
+}
